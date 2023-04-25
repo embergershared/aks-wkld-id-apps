f530cdc0f662363821ec79fefadcb1cb3dbb6c80
Emmanuel Bergerat <45499891+gopher194@users.noreply.github.com>
2023-04-25 14:27:23 -0400

    added akvdotnet

---
+++ akvdotnet/Program.cs
@@ -0,0 +1,45 @@
+// <directives>
+using System;
+using System.Threading;
+using Azure.Security.KeyVault.Secrets;
+// <directives>
+
+namespace akvdotnet
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      Program P = new Program();
+      string keyvaultURL = Environment.GetEnvironmentVariable("KEYVAULT_URL");
+      if (string.IsNullOrEmpty(keyvaultURL))
+      {
+        Console.WriteLine("KEYVAULT_URL environment variable not set");
+        return;
+      }
+
+      string secretName = Environment.GetEnvironmentVariable("SECRET_NAME");
+      if (string.IsNullOrEmpty(secretName))
+      {
+        Console.WriteLine("SECRET_NAME environment variable not set");
+        return;
+      }
+
+      SecretClient client = new SecretClient(
+          new Uri(keyvaultURL),
+          new MyClientAssertionCredential());
+
+      while (true)
+      {
+        Console.WriteLine($"{Environment.NewLine}START {DateTime.UtcNow} ({Environment.MachineName})");
+
+        // <getsecret>
+        var keyvaultSecret = client.GetSecret(secretName).Value;
+        Console.WriteLine("Your secret is: " + keyvaultSecret.Value);
+
+        // sleep and retry periodically
+        Thread.Sleep(600000);
+      }
+    }
+  }
+}
